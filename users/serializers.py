0b71d3ee8eec37f3e9aa99a312048954f5428164
Aleksandr <aqwa06@gmail.com>
2024-11-17 12:00:12 +0500

    fix serializers.py in users

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,22 @@
 from rest_framework import serializers
-from .models import CustomUser
+from .models import CustomUser, Payments
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = '__all__'
-        
+
+
+class PaymentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payments
+        fields = '__all__'
+
+
+class CustomsUserDetailSerializer(serializers.ModelSerializer):
+    payment_history = PaymentsSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'email', 'phone_number', 'avatar', 'city', 'payment_history']
