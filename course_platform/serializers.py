7f4354c7bd127d07d085d2613419dd85f39857dc
Aleksandr <aqwa06@gmail.com>
2024-11-17 12:00:05 +0500

    fix serializers.py in course_platform

--- course_platform/serializers.py
+++ course_platform/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+from rest_framework.fields import SerializerMethodField
 from course_platform.models import Course, Lesson
 
 
@@ -15,3 +15,15 @@ class LessonSerializers(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+
+
+class CourseDetailSerializers(serializers.ModelSerializer):
+    number_of_lessons = SerializerMethodField()
+    lessons = LessonSerializers(many=True, read_only=True)
+
+    def get_number_of_lessons(self, obj):
+        return obj.lessons.count()
+
+    class Meta:
+        model = Course
+        fields = ['id', 'title', 'preview', 'description', 'number_of_lessons', 'lessons']
