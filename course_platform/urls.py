a5d72f1db996b22cfd5aaa1310fc21d715a0cdf2
Aleksandr <aqwa06@gmail.com>
2024-11-26 23:53:24 +0500

    add urls.py in course_platform and users

--- course_platform/urls.py
+++ course_platform/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from course_platform.apps import CoursePlatformConfig
 from course_platform.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
-    LessonUpdateAPIView, LessonDestroyAPIView
+    LessonUpdateAPIView, LessonDestroyAPIView, SubscriptionView
 
 app_name = CoursePlatformConfig.name
 
@@ -16,4 +16,5 @@ urlpatterns = [
     path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_detail'),
     path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson_update'),
     path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson_delete'),
+    path('subscription/', SubscriptionView.as_view(), name='subscription'),
 ] + router.urls
