7dfff648f73e07fa8f39a1744555a6adca6a30f9
Aleksandr <aqwa06@gmail.com>
2024-12-31 02:30:54 +0500

    fix cl.yaml

--- config/settings.py
+++ config/settings.py
@@ -109,11 +109,11 @@ CELERY_BEAT_SCHEDULE = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('POSTGRES_DB') or "${{ secrets.POSTGRES_DB }}",
+        'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST'),
-        'PORT': os.getenv('POSTGRES_PORT'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', "5432"),
     }
 }
 
